09bf728bc112bb87e5a84afc5c556f2df4ba6ecf
GurshaanK <kang501@purdue.edu>
2025-03-20 12:58:24 -0400

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-3x1 font-bold underline bg-blue-500 text-white">
+        Welcome to Home
+      </h1>
+      <section className="h-3 bg-green-400 flex items-center justify-center text-3xl p-12 md:p-14 lg:p-16">
+        <p>lorem ipsum dolor </p>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
